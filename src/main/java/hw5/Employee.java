9579d3f2752c6da4f0d91cd867aa525256c20630
D.Plotnikov <plotnikdima5296@gmail.com>
2022-03-31 21:00:12 +0500

    Initial commit

---
+++ src/main/java/hw5/Employee.java
@@ -0,0 +1,32 @@
+package hw5;
+
+public class Employee {
+    public String name;
+    public String post;
+    public String email;
+    public String phoneNumber;
+    public int salary;
+    public int age;
+
+    public Employee(String name, String post, String email, String phoneNumber, int salary, int age) {
+        this.name = name;
+        this.post = post;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println("Имя: " + name);
+        System.out.println("Должность: " + post);
+        System.out.println("Email: " + email);;
+        System.out.println("Номер телефона: " + phoneNumber);;
+        System.out.println("Зарплата: " + salary);;
+        System.out.println("Возраст: " + age);;
+        System.out.println();
+
+    }
+}
+
+
---
+++ src/main/java/hw5/Main.java
@@ -0,0 +1,30 @@
+package hw5;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employerOne = new Employee("Олег", "консультант", "оleg228@mail.ru", "79081233147", 10000, 54);
+        Employee employerTwo = new Employee("Виталик", "программист", "vital33@mail.ru", "79511566288", 250000, 25);
+        Employee employerThree = new Employee("Ольга", "продавец", "olga333@mail.ru", "79000994651", 25000, 41);
+        Employee employerFour = new Employee("Максим", "фитнесс-тренер", "tcigan@yandex.ru", "79191617185", 70000, 34);
+        Employee employerFive = new Employee("Михаил", "программист", "rafitat@gmail.com", "79038734619", 101000, 25);
+
+
+        Employee[] employeesArr = new Employee[5];
+
+        employeesArr[0] = employerOne;
+        employeesArr[1] = employerTwo;
+        employeesArr[2] = employerThree;
+        employeesArr[3] = employerFour;
+        employeesArr[4] = employerFive;
+
+
+        for (int i = 0; i < employeesArr.length; i++) {
+            if (employeesArr[i].age > 40) {
+                employeesArr[i].printInfo();
+            }
+        }
+    }
+}
